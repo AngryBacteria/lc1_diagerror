bf14444402932aed368654c6b53b1b534e6564d0
AngryBacteria <AngryBacteria@protonmail.com>
2023-03-28 17:02:44 +0200

    init commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+
+<Suspense>
+    <RouterView />
+</Suspense>
+
+</template>
+
+<style scoped>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+const data = await fetch('https://localhost:7139/weatherforecast')
+const json = data.json()
+console.log(json)
+
+</script>
+
+<template>
+  <main>
+    <h1>Hello</h1>
+  </main>
+</template>
