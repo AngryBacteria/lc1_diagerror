cbf162dbd5c76c24c7ac29516768ba290883f8dc
Jorma Steiner <jorma.steiner@students.bfh.ch>
2023-04-09 17:20:03 +0200

    added all database entity classes and a converter class for the enum types, data base is filled with simple items of the entities, also added first set of endpoints for db querys.

--- backend/Models/Model.cs
+++ backend/Models/Model.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace backend.Models
 {
@@ -6,6 +10,7 @@ namespace backend.Models
     {
         public DbSet<Questionnaire> Questionnaires { get; set; } = null!;
         public DbSet<Question> Questions { get; set; } = null!;
+        public DbSet<Answer> Answers { get; set; } = null!;
 
         public string DbPath { get; }
 
@@ -27,17 +32,73 @@ namespace backend.Models
         // The following configures EF to create a Sqlite database file in the "Database" folder inside the project
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        
+
+            modelBuilder.Entity<Answer>()
+                .ToTable(b => b.HasCheckConstraint("CHK_InvitationIdLength", "LENGTH(InvitationId) = 8"));
+
+            modelBuilder.Entity<Questionnaire>()
+                .ToTable(b => b.HasCheckConstraint("CHK_IdentifierLength", "LENGTH(Identifier) = 2"));
+        }
     }
+
+    [JsonConverter(typeof(EnumConverter<Language>))]
+    public enum Language
+    {
+        EN,
+        DE,
+        FR
+    }
+
+    [JsonConverter(typeof(EnumConverter<QuestionType>))]
+    public enum QuestionType
+    {
+        Likert,
+        FreeText,
+        SingleChoice,
+        MultipleChoice
+    }
+
+    [Index(nameof(Identifier), nameof(Language), IsUnique = true)]
     public class Questionnaire
     {
         public int QuestionnaireId { get; set; }
-        public List<Question> Questions { get; set; }
+        public List<Question> Questions { get; set; } = new List<Question>();
+        [MaxLength(2)]
+        public string Identifier { get; set; }
+        public Language Language { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string DescriptionForCustomer { get; set; }
+        public int ValidAfterDays { get; set; }
+        public int ValidForDays { get; set; }
     }
 
     public class Question
     {
         public int QuestionId { get; set; }
         public int QuestionnaireId { get; set; }
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+        [JsonIgnore]
         public Questionnaire Questionnaire { get; set; }
+        public string Text { get; set; }
+        public string Subtext { get; set; }
+        public bool Optional { get; set; }
+        public QuestionType Questiontype { get; set; }
     }
+
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        [JsonIgnore]
+        public Question Question { get; set; }
+        public int QuestionId { get; set; }
+        public string Text { get; set; }
+        public DateOnly Date { get; set; }
+        public string InvitationId { get; set; }
+    }
+
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -3,7 +3,9 @@ using backend.Models;
 using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -78,16 +80,67 @@ app.MapGet("/secured", () =>
     return "Endpoint secured by firebase";
 }).AddEndpointFilter<FirebaseAuthFilter>();
 
-app.MapGet("/question", async (DiagErrorDb db) => await db.Questions.ToListAsync());
+//Endpoints for Questionnaires with answers
+app.MapGet("/questionnaire/complete", async (DiagErrorDb db) => await db.Questionnaires.Include(q => q.Questions).ThenInclude(q => q.Answers).ToListAsync()) ;
 
+app.MapGet("/questionnaire/complete/filter", async (DiagErrorDb db, string id, string identifier, string language) => 
+{
+    var questionnaires = db.Questionnaires
+            .Include(q => q.Questions)
+                .ThenInclude(q => q.Answers)
+            .AsQueryable();
 
-app.MapPost("/question", async (DiagErrorDb db, Question question) =>
+    if (!string.IsNullOrEmpty(id))
+    {
+        questionnaires = questionnaires.Where(q => q.QuestionnaireId == int.Parse(id));
+    }
+
+    if (!string.IsNullOrEmpty(identifier))
+    {
+        questionnaires = questionnaires.Where(q => q.Identifier == identifier);
+    }
+
+    if (!string.IsNullOrEmpty(language))
+    {
+        questionnaires = questionnaires.Where(q => q.Language == Enum.Parse<Language>(language)); //Todo: try/catch parsing
+    }
+
+    return await questionnaires.ToListAsync();
+});
+
+//Endpoints for questionnaires without answers
+app.MapGet("/questionnaire/light", async (DiagErrorDb db) => await db.Questionnaires.Include(q => q.Questions).ToListAsync());
+
+app.MapPost("/questionnaire/light", async (DiagErrorDb db, Questionnaire questionnaire) =>
+{
+    await db.Questionnaires.AddAsync(questionnaire);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/questionnaire/light/{questionnaire.QuestionnaireId}", questionnaire);
+});
+
+//Endpoints for questions with answers
+app.MapGet("/question/complete", async (DiagErrorDb db) => await db.Questions.Include(a => a.Answers).ToListAsync());
+
+//Endpoints for questions without answers
+app.MapGet("/question/light", async (DiagErrorDb db) => await db.Questions.ToListAsync());
+
+app.MapPost("/question/light", async (DiagErrorDb db, Question question) =>
 {
     await db.Questions.AddAsync(question);
     await db.SaveChangesAsync();
-    return Results.Created($"/question/{question.QuestionId}", question);
+    return Results.Created($"/question/light/{question.QuestionId}", question);
 });
 
+//Endpoints for answers
+app.MapGet("/answer", async (DiagErrorDb db) => await db.Answers.ToListAsync());
+
+app.MapPost("/answer", async (DiagErrorDb db, Answer answer) =>
+{
+    await db.Answers.AddAsync(answer);
+    await db.SaveChangesAsync();
+    return Results.Created($"/answer/{answer.AnswerId}", answer);
+});
 
 app.Run();
 
